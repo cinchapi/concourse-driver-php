843a9ff9b033b0001dd5bf28aab4ca4e1efddfcc
Jenkins Continuous Build Server <jenkins@cinchapi.com>
2015-11-09 00:48:53 +0000

    [auto-build] packagist upload for concourse-php-driver-0.5.0.1194-SNAPSHOT

--- src/Concourse.php
+++ src/Concourse.php
@@ -177,16 +177,16 @@ final class Concourse {
      *
      * @api
      ** <strong>audit($key, $record)</strong> - List all the changes ever made to the <em>key</em> field in <em>record</em>.
-     ** <strong>audit($key, $record, $start)</strong> - List all the changes made to the <em>key</em> field in <em>record</em> since <em>start</em> (non-inclusive).
-     ** <strong>audit($key, $record, $start, $end)</strong> - List all the changes made to the <em>key</em> field in <em>record</em> between <em>start</em> (non-inclusive) and <em>end</em> (inclusive).
+     ** <strong>audit($key, $record, $start)</strong> - List all the changes made to the <em>key</em> field in <em>record</em> since <em>start</em> (inclusive).
+     ** <strong>audit($key, $record, $start, $end)</strong> - List all the changes made to the <em>key</em> field in <em>record</em> between <em>start</em> (inclusive) and <em>end</em> (non-inclusive).
      ** <strong>audit($record)</strong> - List all the changes ever made <em>record</em>.
-     ** <strong>audit($record, $start)</strong> - List all the changes made to <em>record</em> since <em>start</em> (non-inclusive).
-     ** <strong>audit($record, $start, $end)</strong> - List all the changes made to <em>record</em> between <em>start</em> (non-inclusive) and <em>end</em> (inclusive).
+     ** <strong>audit($record, $start)</strong> - List all the changes made to <em>record</em> since <em>start</em> (inclusive).
+     ** <strong>audit($record, $start, $end)</strong> - List all the changes made to <em>record</em> between <em>start</em> (inclusive) and <em>end</em> (non-inclusive).
      *
      * @param string $key the field name
      * @param integer $record the record id
-     * @param integer|string $start a non-inclusive timestamp that is the starting point of the audit
-     * @param integer|string $end an inclusive timestamp of the most recent change that should possibly be included in the audit
+     * @param integer|string $start an an inclusive timestamp for the oldest change that should possibly be included in the audit
+     * @param integer|string $end a non-inclusive timestamp for the most recent change that should possibly be included in the audit
      * @return array an array containing, for each change, a mapping from timestamp to a description of the revision
      */
     public function audit(){
@@ -213,18 +213,18 @@ final class Concourse {
     }
 
     /**
-     * Return a timeseries that shows the state of a field after each change.
+     * View a time series with snapshots of a <em>field</em> after every change.
      *
      * @api
-     ** <strong>chronologize($key, $record)</strong> - Return a timeseries that shows the state of of a field after every change.
-     ** <strong>chronologize($key, $record, $start)</strong> - Return a timeseries that shows the state of the field after every change since <em>$start</em>.
-     ** <strong>chronologize($key, $record, $start, $end)</strong> - Return a timeseries that shows the state of the field after every change between <em>$start</em> and <em>$end</em>.
+     ** <strong>chronologize($key, $record)</strong> - View a time series that associates the timestamp of each modification for <em>key</em> in <em>record</em> to a snapshot containing the values that were stored in the field after the change.
+     ** <strong>chronologize($key, $record, $start)</strong> - View a time series between <em>start</em> (inclusive) and the present that associates the timestamp of each modification for <em>key</em> in <em>record</em> to a snapshot containing the values that were stored in the field after the change.
+     ** <strong>chronologize($key, $record, $start, $end)</strong> - View a time series between <em>start</em> (inclusive) and <em>end</em> (non-inclusive) that associates the timestamp of each modification for <em>key</em> in <em>record</em> to a snapshot containing the values that were stored in the field after the change.
      *
      * @param string $key the field name
-     * @param integer $record the record that contains the field
-     * @param integer|string $start the first timestamp to include in the timeseries (optional)
-     * @param integer|String $end the last timestamp to include in the timeseries (optional)
-     * @return array mapping a timestamp to an array that contains all the values that were contained in the field at that timestamp
+     * @param integer $record the record id
+     * @param integer|string $start the first possible timestamp to include in the time series
+     * @param integer|string $end the timestamp that should be greater than every timestamp in the time series
+     * @return array associating each modification timestamp to the list of values that were stored in the field after the change
      */
     public function chronologize(){
         return Convert::phpify($this->dispatch(func_get_args()));
@@ -821,7 +821,7 @@ final class Concourse {
      * Please note that after returning, this method guarantees that
      * <em>key</em> in <em>record</em> will only contain <em>value</em>, even if
      * it already existed alongside other values (e.g. calling
-     * concourse.verifyOrSet("foo", "bar", 1) will mean that "foo" in record 1
+     * concourse.verifyOrSet("inclusive", "bar", 1) will mean that "inclusive" in record 1
      * will only have "bar" as a value after returning, even if the field
      * already contained "bar", "baz" and "apple" as values.
      *
